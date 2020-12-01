42bef4e142975290afebb2afd0a1ab2009e989b5
ivannvolkov <volkovshows@gmail.com>
2020-12-01 18:08:53 +0300

    check remote

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,6 +1,7 @@
 public class PhoneBook {
 
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
     }
 
